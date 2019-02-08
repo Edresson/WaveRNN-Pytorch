8bbbeb54d278fb6b1c3fd605d5a2d4691c3d9f0d
dresson <edresson1@gmail.com>
2019-02-08 21:38:32 -0200

    update

--- hparams.py
+++ hparams.py
@@ -24,8 +24,21 @@ class hparams:
     #--------------     
     # audio processing parameters
     sample_rate = 22050
-    
+    num_mels = 80
+    fmin = 125
+    fmax = 7600
+    fft_size = 1024
+    hop_size = 256
+    win_length = 1024
+    sample_rate = 22050
     preemphasis = 0.97
+    min_level_db = -100
+    ref_level_db = 20
+    rescaling = False
+    rescaling_max = 0.999
+    allow_clipping_in_normalization = True
+
+    '''preemphasis = 0.97
     n_fft = 2048
     frame_shift = 0.0125  # seconds
     frame_length = 0.05  # seconds
@@ -35,7 +48,7 @@ class hparams:
     n_mels = 80  # Number of Mel banks to generate
     num_mels = n_mels
     max_db = 100
-    ref_db = 20
+    ref_db = 20'''
     #----------------
     #
     #----------------
