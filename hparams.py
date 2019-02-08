c2228aafe102fc7fce9fb01e6e90454403698721
dresson <edresson1@gmail.com>
2019-02-08 17:05:18 -0200

    update

--- hparams.py
+++ hparams.py
@@ -28,9 +28,9 @@ class hparams:
     n_fft = 2048
     frame_shift = 0.0125  # seconds
     frame_length = 0.05  # seconds
-    hop_length = int(sr * frame_shift)  # samples. =276.
+    hop_length = int(sample_rate * frame_shift)  # samples. =276.
     hop_size = hop_length 
-    win_length = int(sr * frame_length)  # samples. =1102.
+    win_length = int(sample_rate * frame_length)  # samples. =1102.
     n_mels = 80  # Number of Mel banks to generate
     num_mels = n_mels
     max_db = 100
