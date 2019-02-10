6d8e7afc4fff22e957cb16f1e87e15d189b405dd
dresson <edresson1@gmail.com>
2019-02-09 22:02:29 -0200

    update

--- hparams.py
+++ hparams.py
@@ -64,7 +64,7 @@ class hparams:
     # if necessary (i.e 4 x 4 x 16 = 256)
     #upsample_factors = (4, 4, 16)
     #new
-    upsample_factors = (3, 4, 23)  # if necessary (i.e 3 x 4 x 23 = 276)
+    upsample_factors = (3, 4, 22)  # if necessary (i.e 3 x 4 x 23 = 276)
     compute_dims = 128
     res_out_dims = 128
     res_blocks = 10
