1bdf61856265bea9309d95b04141093b194d0b3f
dresson <edresson1@gmail.com>
2019-02-21 14:31:13 -0300

    update

--- hparams.py
+++ hparams.py
@@ -56,7 +56,7 @@ class hparams:
     #
     #----------------
     # training parameters
-    batch_size = 128
+    batch_size = 94
     nepochs = 5000
     save_every_step = 10000
     evaluate_every_step = 5000
