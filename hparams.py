122fc81573367f6cd0960c93e7c385b9e07cf91a
dresson <edresson1@gmail.com>
2019-05-09 19:03:19 -0300

    update

--- hparams.py
+++ hparams.py
@@ -58,8 +58,8 @@ class hparams:
     # training parameters
     batch_size = 128
     nepochs = 15000
-    save_every_step = 10 #10000
-    evaluate_every_step = 10
+    save_every_step = 10000
+    evaluate_every_step = 5000
     # seq_len_factor can be adjusted to increase training sequence length (will increase GPU usage)
     seq_len_factor = 5
     seq_len = seq_len_factor * hop_size
